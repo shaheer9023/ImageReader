6fc6586140aa14cf8176c2ff3612ad649070df9a
shaheer9023 <shaheerahmad9023@gmail.com>
2025-02-24 16:37:59 +0500

    app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API key configuration
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("KEY")
 if not GOOGLE_API_KEY:
     raise ValueError("GOOGLE_API_KEY environment variable not found")
 genai.configure(api_key=GOOGLE_API_KEY)
